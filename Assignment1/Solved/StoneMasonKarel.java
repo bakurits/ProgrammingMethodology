f7b1359da65e06c51ae82ed28d2287660efd69c7
bakuri <bakuricucxashvili@gmail.com>
2016-10-23 13:56:37 +0400

    added StoneMasonKarel.java

---
+++ Assignment1/Solved/StoneMasonKarel.java
@@ -0,0 +1,60 @@
+/*
+ * File: StoneMasonKarel.java
+ * --------------------------
+ * The StoneMasonKarel subclass as it appears here does nothing.
+ * When you finish writing it, it should solve the "repair the quad"
+ * problem from Assignment 1.  In addition to editing the program,
+ * you should be sure to edit this comment so that it no longer
+ * indicates that the program does nothing.
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+	
+	/* this method moves karel forward if it's possible */
+	private void moveIfItsPossible() {
+		if (frontIsClear()) {
+			move();
+		}
+	}
+	
+	/* this method moves Karel back to the masons's socle */
+	private void backToBegin() {
+		while (frontIsClear()) {
+			move();
+		}
+	}
+	
+	/* this method fixes current mason */
+	private void fixCurrentMason() {
+		while (frontIsClear()) {
+			if (!beepersPresent()) {
+				putBeeper();
+			}
+			move();
+		}
+		if (!beepersPresent()) {
+			putBeeper();
+		}
+	}
+	
+	/* this method fixes all masons */
+	private void fixTheMasons () {
+		while (frontIsClear()) {
+			turnLeft();
+			fixCurrentMason();
+			turnAround();
+			backToBegin();
+			turnLeft();
+			moveIfItsPossible();
+			moveIfItsPossible();
+			moveIfItsPossible();
+			moveIfItsPossible();
+		}
+	}
+	
+	public void run() {
+		fixTheMasons();
+	}
+}
